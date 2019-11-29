293249fcc490ff74825ccd778181e225c87c55e8
Riley Moore <rilemoore@gmail.com>
2019-11-29 21:10:41 +0000

    Automatically backed up by Learn

--- config/environment.rb
+++ config/environment.rb
@@ -1,10 +1,12 @@
 ENV['SINATRA_ENV'] ||= "development"
 
-configure :development do
-  set :database, 'sqlite3:db/database.db'
-end
+
 
 require 'bundler/setup'
 Bundler.require(:default, ENV['SINATRA_ENV'])
+
+configure :development do
+  set :database, 'sqlite3:db/database.db'
+end
 
 require './app'
